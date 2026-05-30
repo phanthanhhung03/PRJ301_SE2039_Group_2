469173d2cdfdcd7d78e73cbda1293812567545fa
longvan-shn <longvan4869@gmail.com>
2026-05-31 03:10:57 +0700

    them func createCus va getCus(phone)

--- src/java/dao/CustomerDAO.java
+++ src/java/dao/CustomerDAO.java
@@ -4,6 +4,7 @@
  */
 package dao;
 
+import dbutils.DBUtils;
 import dto.Customer;
 import dto.CustomerTier;
 import java.sql.Connection;
@@ -17,6 +18,38 @@ import java.sql.ResultSet;
  */
 public class CustomerDAO {
 
+    public int createCustomer(Customer c){
+        int result=0;
+        Connection cn=null;
+        try {
+            //buoc 1: make connection
+            cn=DBUtils.getConnection();
+            if(cn!=null){
+                //buoc 2 : viet sql
+                String sql = "insert dbo.Customers([FullName], [PhoneNumber], [Email], [Password], [Address])\n"
+                        + "values(?, ?, ?, ?, ?)";
+                PreparedStatement st=cn.prepareStatement(sql);
+                st.setString(1, c.getFullName());
+                st.setString(2, c.getPhoneNumber());
+                st.setString(3, c.getEmail());
+                st.setString(4, c.getPassword());
+                st.setString(5, c.getAddress());
+                result=st.executeUpdate();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                //buoc 4
+                if(cn!=null) 
+                    cn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+    
     public Customer getCustomer(String email, String password) {
         Customer result = null;
         Connection cn = null;
@@ -43,7 +76,7 @@ public class CustomerDAO {
                         + "t.PriorityLevel, "
                         + "t.BookingWindowDays "
                         + "FROM Customers c " + "JOIN CustomerTiers t " + "ON c.TierID = t.TierID " 
-                        + "WHERE c.Email = ? " + "AND c.PasswordHash = ?";
+                        + "WHERE c.Email = ? " + "AND c.Password = ?";
 
                 // Step 3
                 st = cn.prepareStatement(sql);
@@ -182,4 +215,76 @@ public class CustomerDAO {
         }
         return result;
     }
+    
+    public Customer getCustomer2(String phone) {
+        Customer result = null;
+        Connection cn = null;
+        PreparedStatement st = null;
+        ResultSet table = null;
+
+        try {
+            // Step 1: Connect DB
+            cn = dbutils.DBUtils.getConnection();
+
+            if (cn == null) {
+                System.out.println("not connect");
+            }
+
+            if (cn != null) {
+
+                // Step 2: SQL
+                String sql = "SELECT CustomerID, FullName, Email, Address, "
+                        + "TierID, CurrentPoints, TotalBookings, TotalSpend, "
+                        + "Status, CreatedAt "
+                        + "FROM Customers "
+                        + "WHERE PhoneNumber = ?";
+
+                // Step 3
+                st = cn.prepareStatement(sql);
+
+                st.setString(1, phone);
+
+                // Step 4
+                table = st.executeQuery();
+
+                if (table.next()) {
+
+                    result = new Customer();
+
+                    result.setCusId(table.getInt("CustomerID"));
+                    result.setFullName(table.getString("FullName"));
+                    result.setPhoneNumber(phone);
+                    result.setEmail(table.getString("Email"));
+                    result.setAddress(table.getString("Address"));
+                    result.setCurrentPoint(table.getInt("CurrentPoints"));
+                    result.setTotalBooking(table.getInt("TotalBookings"));
+                    result.setTotalSpend(table.getDouble("TotalSpend"));
+                    result.setStatus(table.getBoolean("Status"));
+                    result.setCreatedAt(table.getDate("CreatedAt"));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+
+                if (table != null) {
+                    table.close();
+                }
+
+                if (st != null) {
+                    st.close();
+                }
+
+                if (cn != null) {
+                    cn.close();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
 }
+
